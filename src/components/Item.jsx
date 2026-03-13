dbd7f88054e268aea363d198385085b7f244d316
Deepika <laxmisipika98@gmail.com>
2026-03-13 23:02:01 +0545

    Initial React project

---
+++ src/components/Item.jsx
@@ -0,0 +1,15 @@
+import SingleItem from "./singleItem";
+
+import "./Item.css";
+
+const Items = ({ items }) => {
+  return (
+    <div className="items">
+      {items.map((item) => {
+        return <SingleItem key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+
+export default Items;
